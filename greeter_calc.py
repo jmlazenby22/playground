9385ef882cb994904f54b000289da62d55710f29
jmlazenby22 <jmlazenby22@gmail.com>
2025-09-03 20:36:33 -0700

    Lesson 1: input/print/type practice

---
+++ greeter_calc.py
@@ -0,0 +1,10 @@
+name = input("Your name: ")
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+sum_ = a + b
+prod = a * b
+
+print(f"Nice to meet you, {name}!")
+print(f"{a} + {b} = {sum_}")
+print(f"{a} * {b} = {prod}")
